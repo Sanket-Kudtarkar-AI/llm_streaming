9dc0ef8acac591dbd47829934b6f884c8c1745e2
Sanket <skudtarkar16@gmail.com>
2024-04-16 23:07:28 +0530

    code cleaning

--- test_websocket.py
+++ test_websocket.py
@@ -72,8 +72,6 @@ def handle_connect():
 
 @socketio.on('messagefromclient')
 def handle_message(payload):
-    payload = payload.json()
-    print("message triggered", flush=True)
     query = payload["query"]
 
     sessionid = payload["sessionid"]
